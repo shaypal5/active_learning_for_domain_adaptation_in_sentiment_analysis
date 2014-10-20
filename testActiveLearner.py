b2430b50358cf64e131152705e5a753d9af55781
inbarAndTheBadSeeds <inbar.naor1@gmail.com>
2014-10-20 11:49:18 +0300

    testActiveLearner now uses the BlitzerDatasetDomain enum

--- testActiveLearner.py
+++ testActiveLearner.py
@@ -8,6 +8,7 @@ Created on Wed Oct 15 18:33:19 2014
 from UncertaintySampleSelector import UncertaintySampleSelector
 from QueryByPartialDataCommiteeSampleSelector import QueryByPartialDataCommiteeSampleSelector
 from TargetAndSourceQBCSampleSelector import TargetAndSourceQBCSampleSelector
+from BlitzerDatasetDomain import BlitzerDatasetDomain
 
 import ActiveLearner
 import parseProcessedDataFileForScikit
@@ -16,31 +17,23 @@ from sklearn.svm import LinearSVC
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.preprocessing import LabelEncoder
 
-sourceFolderPath = "C:/OrZuk/Data/sorted_data/apparel/"
-targetFolderPath = "C:/OrZuk/Data/sorted_data/jewelry_&_watches/"
-trainFileName = "processed.review.trainset"
-testFileName = "processed.review.testset"
+sourceDomain = BlitzerDatasetDomain.apparel
+targetDomain = BlitzerDatasetDomain.jewelry
 
-#Parsing train and test data for domain 1 - Apparel
-trainXsource, trainYsource  = parseProcessedDataFileForScikit.parseDataFile(sourceFolderPath+trainFileName)
-testXsource, testYsource = parseProcessedDataFileForScikit.parseDataFile(sourceFolderPath+testFileName)
+#Parsing train and test data for source domain
+trainXsource, trainYsource  = parseProcessedDataFileForScikit.parseDataFile(sourceDomain.getTrainFileFullPath())
+testXsource, testYsource = parseProcessedDataFileForScikit.parseDataFile(sourceDomain.getTestFileFullPath())
 trainSourceSize = len(trainXsource)
 vectorizer = DictVectorizer(dtype=float, sparse=True)
 encoder = LabelEncoder()
 
-#vectorize!
-#newTrainXapparel = vectorizer.fit_transform(trainXapparel)
-#newTrainYapparel = encoder.fit_transform(trainYapparel)
-
-#Parsing train and test data for domain 2 - Jewlery
-trainXtarget, trainYtarget  = parseProcessedDataFileForScikit.parseDataFile(targetFolderPath+trainFileName)
-testXtarget, testYtarget = parseProcessedDataFileForScikit.parseDataFile(targetFolderPath+testFileName)
+#Parsing train and test data for target domain
+trainXtarget, trainYtarget  = parseProcessedDataFileForScikit.parseDataFile(targetDomain.getTrainFileFullPath())
+testXtarget, testYtarget = parseProcessedDataFileForScikit.parseDataFile(targetDomain.getTestFileFullPath())
 trainTargetSize = len(trainXtarget)
 print(type(trainXtarget))
 
 #vectorize!
-#newTrainXjewlery = vectorizer.transform(trainXjewlery)
-#newTrainYjewlery = encoder.transform(trainYjewlery)
 vectorized = vectorizer.fit_transform(trainXsource+trainXtarget)
 vectorizedLabels = encoder.fit_transform(trainYsource+trainYtarget)
 total = trainSourceSize+trainTargetSize
