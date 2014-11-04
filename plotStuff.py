2416e9a5f29454cf353736adeb206ff55eea39e2
shaypal5 <shaypal5@gmail.com>
2014-11-04 13:24:26 +0200

    More plots

--- plotStuff.py
+++ plotStuff.py
@@ -14,8 +14,8 @@ uncAcuPlt.plot(values, [0.8876 for i in range(12)], 'b--')
 UncAcu = [None,0.7388,0.7807,0.7807,0.7997,0.8024,0.824,0.8234,0.8376,0.8477,0.8518,0.8484]
 uncAcuPlt.plot(values, UncAcu)
 plt.ylim(0.5,1)
-uncAcuPlt.set_xlabel('Number of instances')
-uncAcuPlt.set_ylabel('Accuracy')
+uncAcuPlt.set_xlabel('Number of instances', color='k')
+uncAcuPlt.set_ylabel('Accuracy', color='k')
 plt.show()
 
 with plt.style.context('fivethirtyeight'):
@@ -24,6 +24,7 @@ with plt.style.context('fivethirtyeight'):
     ind = np.arange(N)  # the x locations for the groups
     width = 0.2       # the width of the bars
     fig, ax = plt.subplots()
+    fig.set_size_inches(9,5)
     
     sourceAcc = (0.9674, 1.084, 1.012048193, 0.978082824, 0.879955539, 0.892857143, 0.91954023, 0.831460674)
     rects1 = ax.bar(ind, sourceAcc, width, color='#E24A33' )
@@ -35,17 +36,22 @@ with plt.style.context('fivethirtyeight'):
     rects4 = ax.bar(ind+3*width, stqbcAcc, width)#, color='#467821')
     
     # add some text for labels, title and axes ticks
-    ax.set_ylabel('Scores')
-    ax.set_title('Scores by group and gender')
-    ax.set_xticks(ind+3*width)
-    ax.set_xticklabels( ('G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8') )
+    ax.set_ylabel('Relative Accuracy', color='k', fontsize=12)
+    #ax.set_title('Scores by group and gender', color='k')
+    ax.set_xticks(ind+2*width)
+    ax.set_xticklabels( ('Similar \n Big To Small', 'Different', 'Similar \n Big To Big', 'Different', 'Similar \n Small To Small', 'Different', 'Similar \n Small To Big', 'Different') , color='k', fontsize=12)
     
-    ax.legend( (rects1[0], rects2[0], rects3[0], rects4[0]), ('SourceSVM', 'Uncertainty', 'partialQBC', 'STQBC') )
+    lg = ax.legend( (rects1[0], rects2[0], rects3[0], rects4[0]), ('SourceSVM', 'Uncertainty', 'partialQBC', 'STQBC') )
+    lg.draw_frame(True)
+    frame = lg.get_frame()
+    frame.set_ec('0.45')
+    frame.set_linewidth(1)
+    ax.axhline(y=1, linewidth=2, color='k', zorder=0, ls='dashed')
     
     #autolabel(rects1)
     #autolabel(rects2)
     #autolabel(rects2)
     #autolabel(rects2)
-    plt.ylim(0.5,1.1)
+    plt.ylim(0.8,1.1)
     plt.show()
 
